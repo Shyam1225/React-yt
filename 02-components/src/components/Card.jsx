fee7119a26e939e348ec2b5c0f9203174c786a2e
Shyam1225 <damanihit@gmail.com>
2025-11-29 15:54:35 +0530

    02-components

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="card">
+        <h1>Shyam</h1>
+        <p>I am a developer</p>
+    </div>
+  )
+}
+
+export default Card
